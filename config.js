b3c95b1afa7a250177f090e42e97b54b24d26423
Jonathan Nimmo <jnimmo@electronet.co.nz>
2022-09-25 22:12:41 +1300

    correct the broken url

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_url:"http://mods.battlenetwork.io/"
+    api_url:"http://mods.battlenetwork.io"
 }
 //when developing with local API update this to http://localhost:80
 export {config}

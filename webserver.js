cdc45a84fa47bde224f86266375b5b8c854a59aa
Jonathan Nimmo <jnimmo@electronet.co.nz>
2022-09-25 21:52:02 +1300

    move website to root

--- webserver.js
+++ webserver.js
@@ -1,9 +1,10 @@
+//test server for running with express
 const express = require('express')
 const app = express()
 const port = 3000
 
 
-app.use(express.static('web_static'))
+app.use(express.static('.'))
 
 app.listen(port)
 console.log(`webserver hosted on http://localhost:${port}`)

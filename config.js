f16949a83f81cb53090f134ed1aa61582cd00083
Keristero <keristero@gmail.com>
2023-02-19 11:20:35 +1300

    use new modlister api

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_url:"http://mods.battlenetwork.io"
+    api_url:"https://159.223.186.140:8080"
 }
 //when developing with local API update this to http://localhost:80
 export {config}

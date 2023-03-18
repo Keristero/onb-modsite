21cd55c589f4975b56506e37649e5ce2a0193a7f
Keristero <10967222+Keristero@users.noreply.github.com>
2023-03-18 21:36:42 +1300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_url:"https://battlenetwork.io:8080"
+    api_url:"https://keristero.xyz"
 }
 //when developing with local API update this to http://localhost:80
 export {config}

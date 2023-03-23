29d777bb8c242d1050677c88711699fe274cb7a1
Keristero <10967222+Keristero@users.noreply.github.com>
2023-03-23 14:59:19 +1300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    api_url:"https://keristero.xyz"
+    api_url:"https://www.keristero.xyz"
 }
 //when developing with local API update this to http://localhost:80
 export {config}

f94bf6e3c72f65a5fccc908b0d6f8aa26b4827ae
Keristero <keristero@gmail.com>
2023-08-22 22:24:42 +1200

    opps, disable debug mode

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
     websocet_api_url:"wss://www.keristero.xyz",
     api_url:"https://www.keristero.xyz",
-    debug:true
+    debug:false
 }
 //when developing with local API update this to http://localhost:80
 export {config}

6504e219fe40a4428696bd9aaa143044c3f73109
Keristero <keristero@gmail.com>
2024-02-22 21:33:22 +1300

    add vite

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    build: {
+      target: 'esnext',
+      // other build options...
+    },
+    // other Vite options...
+  });

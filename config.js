93a733ee5de25731f348b557f9c1c0299602aa73
Keristero <10967222+Keristero@users.noreply.github.com>
2025-08-19 15:18:01 +1200

    Update config.js
    try using new domain

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
-    websocet_api_url:"wss://www.keristero.xyz",
-    api_url:"https://www.keristero.xyz",
+    websocet_api_url:"wss://api.onb.keristero.com",
+    api_url:"https://api.onb.keristero.com",
     debug:false
 }
 //when developing with local API update this to http://localhost:80
